0dff9d0fb0cc372c6062115b953fb416520ca9c0
Junsu <murane@naver.com>
2021-06-09 18:06:30 +0900

    feat.account : 계좌 정보 수정 테스트 작성

--- service/src/test/kotlin/account/AccountServiceSpec.kt
+++ service/src/test/kotlin/account/AccountServiceSpec.kt
@@ -3,6 +3,7 @@ package settlement.kotlin.service.account
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.FeatureSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
@@ -13,6 +14,7 @@ import settlement.kotlin.db.owner.Owner
 import settlement.kotlin.db.owner.OwnerRepository
 import settlement.kotlin.service.owner.AccountService
 import settlement.kotlin.service.owner.req.CreateAccountRequest
+import settlement.kotlin.service.owner.req.ModifyAccountRequest
 import java.util.*
 
 class AccountServiceSpec : FeatureSpec() {
@@ -24,6 +26,7 @@ class AccountServiceSpec : FeatureSpec() {
         ownerRepository = ownerRepository
     )
 
+    private val accountIdSlot = slot<Long>()
     private val ownerIdSlot = slot<Long>()
     private val bankSlot = slot<String>()
     private val bankAccountSlot = slot<String>()
@@ -120,8 +123,61 @@ class AccountServiceSpec : FeatureSpec() {
         }
 
         feature("계좌 정보 수정 기능"){
-            xscenario("정상적으로 계좌정보를 수정하였다."){
 
+            every {
+                ownerRepository.findById(capture(ownerIdSlot))
+            }.answers{
+                if(ownerIdSlot.captured==testOwner.id) Optional.of(testOwner)
+                else Optional.empty()
+            }
+
+            every {
+                accountRepository.findById(capture(accountIdSlot))
+            }.answers{
+                if(accountIdSlot.captured == 404L) Optional.empty()
+                else Optional.of(testAccount)
+            }
+
+            scenario("오너가 존재하지 않을경우 예외를 발생시킨다."){
+                val req = ModifyAccountRequest(
+                    ownerId = 404L,
+                    id = 1L,
+                    bank = "testBank",
+                    bankAccount = "testAccount",
+                    accountHolder = "testAccountHolder"
+                )
+                shouldThrowExactly<RuntimeException> {
+                    accountService.modifyAccount(req)
+                }
+            }
+
+            scenario("계좌정보가 존재하지 않을경우 예외를 발생시킨다."){
+                val req = ModifyAccountRequest(
+                    ownerId = 1L,
+                    id = 404L,
+                    bank = "testBank",
+                    bankAccount = "testAccount",
+                    accountHolder = "testAccountHolder"
+                )
+                shouldThrowExactly<RuntimeException> {
+                    accountService.modifyAccount(req)
+                }
+            }
+
+            scenario("정상적으로 계좌정보를 수정하였다."){
+                val req = ModifyAccountRequest(
+                    ownerId = 1L,
+                    id = 1L,
+                    bank = "modifyBank",
+                    bankAccount = "modifyBankAccount",
+                    accountHolder = "modifyAccountHoler"
+                )
+
+                val result = accountService.modifyAccount(req)
+
+                result.bank shouldBe req.bank
+                result.bankAccount shouldBe req.bankAccount
+                result.accountHolder shouldBe  req.accountHolder
             }
         }
     }
